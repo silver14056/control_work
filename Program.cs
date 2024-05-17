e3db440b8dd5912f2287aee81c1972716a09638f
Serg Litov <Serg@litov.com>
2024-05-17 07:57:23 +0300

    Реализован метод генерации массива из случайных строк

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿void Main()
 {
     int arraySize = ReadInt("Введите размер массива: ");
-    
+    string[] array = GenerateArray(arraySize);
 }
 
 int ReadInt(string msg)
@@ -10,6 +10,15 @@ int ReadInt(string msg)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-
+string[] GenerateArray(int size)
+{
+    int[] tempArray = new int[size];
+    
+    for (int i = 0; i < size; ++i)
+    {
+        tempArray[i] = "string element";
+    }
+    return tempArray;
+}
 
 Main();
